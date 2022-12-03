d94e7336f878369eaa7e270591b60b7e87d651a1
unknown <marvelw88888@gmail.com>
2022-12-03 22:02:21 +0700

    interface detail tranksaksi

--- resources/views/client/user/detailtrans.blade.php
+++ resources/views/client/user/detailtrans.blade.php
@@ -1,25 +1,32 @@
 @extends('layouts.layout')
 @section('content')
-    <table border="1">
-        <thead>
-           <tr>
-            <th>Nama Menu</th>
-            <th>Kategori</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Subtotal</th>
-           </tr>
-        </thead>
-        <tbody>
-            @foreach ($details as $val )
-            <tr>
-                <td>{{$val->name_menu}}</td>
-                <td>{{$val->Menus->Kategories->name}}</td>
-                <td>{{$val->price}}</td>
-                <td>{{$val->quantity}}</td>
-                <td>{{$val->subtotal}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="product6">
+    <h1>Detail Tranksaksi</h1>
+        <div class="card-body">
+            <table class="table responsive table-dark" id="tableHasil">
+                <thead class="thead-dark">
+                <tr>
+                    <th>Nama Menu</th>
+                    <th>Kategori</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Subtotal</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach ($details as $val )
+                    <tr>
+                        <td>{{$val->name_menu}}</td>
+                        <td>{{$val->Menus->Kategories->name}}</td>
+                        <td>{{$val->price}}</td>
+                        <td>{{$val->quantity}}</td>
+                        <td>{{$val->subtotal}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <a href="{{url('home/user/profile')}}"><button class="btn btn-primary" style="width:100%;">Back To Profile</button></a>
+        </div>
+    </div>
+
 @endsection
